81e67100ed424cdc401898ac3b1b64334de79085
OVENGEE <mpumelelokenneth911@gmail.com>
2026-07-30 05:11:34 +0200

    working on the shape spawning

--- DoubleTetris/Assets/Scripts/RegularTetrisScripts/TetrominoSpawner.cs
+++ DoubleTetris/Assets/Scripts/RegularTetrisScripts/TetrominoSpawner.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 public class TetrominoSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("In-Game Spawn Settings")]
+    [Tooltip("All 7 tetro prefabes")]
+    public GameObject[] tetrominoPrefabs;
+    [Tooltip("would-space point where the next pies ")]
+    public Transform previewSpawnPoint;
+
+    private int nextIndex;
+    private GameObject currentPreview;
+
     void Start()
     {
-        
+        nextIndex = Random.range(0, tetrominoPrefabs.Length);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnNewTetromino()
     {
+        Vector3 spawnPos = new Vector3(5,19,0);
+        GameObject go = Instantiate(tetrominoPrefabs[nextIndex], spawnPos, Quaternion.identity);
+
+        var tet = go.GetComponent<Tetromino>();
+        tet.fallTime = GameManager.Instance.currentFallTime;
+
         
+
     }
 }
