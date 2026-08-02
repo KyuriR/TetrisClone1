b822124eea3281227fbb105d084d104038661ef2
KMJ-Mahlangu <kmjkamogelo@gmail.com>
2026-08-02 12:25:41 +0200

    Falling mechanic

--- DoubleTetris/Assets/Scripts/BoardII.cs
+++ DoubleTetris/Assets/Scripts/BoardII.cs
@@ -5,6 +5,7 @@ public class BoardII : MonoBehaviour
 {
     public Tilemap tilemap;
 
+
     [Header("Tetrominoes")]
     public TetrominoDataII[] tetrominoes;
 
@@ -14,6 +15,9 @@ public class BoardII : MonoBehaviour
 
     public Vector3Int leftSpawn = new Vector3Int(-3, 9, 0);
     public Vector3Int rightSpawn = new Vector3Int(3, 9, 0);
+
+    public int boardWidth = 10;
+    public int boardHeight = 20;
 
     private void Awake()
     {
@@ -31,5 +35,27 @@ public class BoardII : MonoBehaviour
     {
         TetrominoDataII data = tetrominoes[Random.Range(0, tetrominoes.Length)];
         piece.Initialize(this, spawnPosition, data);
+    }
+
+    public bool IsValidPosition(PieceII piece, Vector3Int position)
+    {
+        foreach (Vector2Int cell in piece.Data.cells)
+        {
+            Vector3Int tilePosition = position + (Vector3Int)cell;
+
+            // Left wall
+            if (tilePosition.x < -boardWidth / 2)
+                return false;
+
+            // Right wall
+            if (tilePosition.x >= boardWidth / 2)
+                return false;
+
+            // Bottom
+            if (tilePosition.y < 0)
+                return false;
+        }
+
+        return true;
     }
 }
